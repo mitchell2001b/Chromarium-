77ebf0e75f288cff5af14419ad72e8a7ede9cd0b
Last,Jochem J.J.A <45848255+JLastig@users.noreply.github.com>
2022-10-03 13:06:51 +0200

    Elemental Affinities

--- yog/Assets/scripts/enemy/EnemyHealth.cs
+++ yog/Assets/scripts/enemy/EnemyHealth.cs
@@ -5,8 +5,18 @@ using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int hitPoints { get; private set; }
+    public float maxHealth = 100f;
+    [SerializeField]
+    AmmoType elementalAffinity = AmmoType.Regular;
+
+    [SerializeField]
+    [Range(0, 1)]
+    float resistanceModifier = .5f;
+
+    [SerializeField]
+    [Range(1, 2)]
+    float weaknessModifier = 1.5f;
+    public float hitPoints { get; private set; }
     public bool isDead { get; private set; } = false;
 
     void Start()
@@ -14,9 +24,44 @@ public class EnemyHealth : MonoBehaviour
         hitPoints = maxHealth;
     }
 
-    public void TakeDamage(int damageAmount)
+    public void RecieveDamage(float damageAmount, AmmoType ammoTypeUsed)
     {
-        hitPoints -= damageAmount;
+        if (elementalAffinity == AmmoType.Regular) hitPoints -= damageAmount;
+        else if (elementalAffinity == ammoTypeUsed) hitPoints -= damageAmount * resistanceModifier;
+        else
+        {
+            switch (elementalAffinity)
+            {
+                case AmmoType.Fire:
+                    if (ammoTypeUsed == AmmoType.Ice) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                case AmmoType.Ice:
+                    if (ammoTypeUsed == AmmoType.Fire) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                case AmmoType.Earth:
+                    if (ammoTypeUsed == AmmoType.Wind) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                case AmmoType.Wind:
+                    if (ammoTypeUsed == AmmoType.Earth) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                case AmmoType.Holy:
+                    if (ammoTypeUsed == AmmoType.Void) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                case AmmoType.Void:
+                    if (ammoTypeUsed == AmmoType.Holy) hitPoints -= damageAmount * weaknessModifier;
+                    else hitPoints -= damageAmount;
+                    break;
+                default:
+                    hitPoints -= damageAmount;
+                    Debug.Log("Couldn't find elemental relation");
+                    break;
+            }
+        }
 
         if(hitPoints <= 0)
         {
